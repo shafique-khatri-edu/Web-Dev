518cd7514f528d3e38d73b5f60937fdcb11453fd
shafique-khatri-edu <shafique.khatri.edu@gmail.com>
2019-01-21 02:18:31 +0530

    add the code from udemy class

--- 1/script.js
+++ 1/script.js
@@ -1,3 +1,54 @@
 
 
+/*
+// Declaring a variable ;
+var firstName = "shafique";
+var lastName = "khatri";
+
+console.log(firstName + " " + lastName);
+
+var shafiqueAge, aftabAge, sabaAge;
+
+shafiqueAge = 21;
+aftabAge = 19;
+sabaAge = 19;
+
+// logical oprater 
+
+var isShafiqueOler = shafiqueAge > aftabAge;
+
+console.log(isShafiqueOler);
+
+
+// type off system to the type of datatype the variable is holding 
+
+console.log(typeof shafiqueAge);
+console.log(typeof aftabAge);
+console.log(typeof firstName);
+console.log(typeof isShafiqueOler);
+
+*/
+
+// oratin precidence 
+// refer the table 
+
+var x, y;
+
+x = y = (3 + 5) * 4 - 6; // 8 * 4 - 6 // 32 - 6 // 26 
+
+console.log(x, y);
+
+x = x * 2;
+x *= 2;
+
+console.log(x);
+
+x = x++; // same as x = x + 1 
+x = x--; // same as x = x - 1 
+
+// even simpler is 
+
+x++ // same as x = x + 1 
+x-- // same as x = x - 1
+
 
